b0900dc0bf55b722bf41be1addd59f14e9c1ca8e
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-09-09 15:23:30 +0100

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 /**
 * main - Entry point
 *
-* Return: 0
+* Return: 1
 */
 int main(void)
 {
-char ch = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, ch);
+char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, ch, sizeof(ch));
 return (1);
 }
