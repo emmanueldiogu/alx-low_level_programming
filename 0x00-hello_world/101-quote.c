5f4e32c69464952c9b61a47395a61ee029255bea
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-09-10 00:05:15 +0100

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
 * main - Entry point
 *
-* Return: 0
+* Return: 1
 */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, ch, sizeof(ch));
 return (1);
 }
