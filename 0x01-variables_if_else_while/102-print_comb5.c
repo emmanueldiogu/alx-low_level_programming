90e03affd25fca75999b23185c2c4c4163d8e297
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-09-12 17:33:42 +0100

    print comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,25 +3,38 @@
  * main - Entry point
  *
  * Return: 0
-*/
+ */
 int main(void)
 {
 int x = 0;
-int y;
+int y, z, a;
 while (x <= 9)
 {
 y = 0;
 while (y <= 9)
 {
-if (x != y && x < y)
+z = 0;
+while (z <= 9)
+{
+a = 0;
+while (a <= 9)
+{
+if (!(x == y && z == y))
 {
 putchar('0' + x);
 putchar('0' + y);
-if ((y + x) != 17)
+putchar(' ');
+putchar('0' + z);
+putchar('0' + a);
+if (!((y + x) == 17 && (z + a) == 17 && z == 9))
 {
 putchar(',');
 putchar(' ');
 }
+}
+a++;
+}
+z++;
 }
 y++;
 }
