79c1e808abab40ab0572278465d2a1506a5daf73
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-09-14 05:44:03 +0100

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
 /**
- * _islower() - Entry point
- * c - parameter to be checked
+ * _islower: Entry point
+ * 'c': parameter to be checked
  * Description: Return 1 if lowercase else return 0
- * Description: _islower() - check for lower case
- * Description: "c" - parameter to check
  * Return: 0
 */
 
