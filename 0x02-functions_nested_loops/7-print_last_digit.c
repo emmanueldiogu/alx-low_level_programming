1ca27a17fb29265f9111bc4702e03eecad7a061f
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-09-14 10:22:59 +0100

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,10 +7,21 @@
  * Description: Function that prints the last digit of a number
  *
  * Return: 0
-*/
+ */
 
 int print_last_digit(int n)
 {
+if (n < 0)
+{
+n = (n * (-1)) % 10;
+}
+else
+{
+n = n % 10;
+}
+_putchar(n + '0');
+return (n);
+
 n = n % 10;
 return (n);
 }
