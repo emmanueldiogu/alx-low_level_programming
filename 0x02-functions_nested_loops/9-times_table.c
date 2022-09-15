3e52fd8368bab8ba56b4ac9eecc389fe52ff5477
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-09-15 20:55:28 +0100

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,37 +8,37 @@
  */
 void times_table(void)
 {
-    int mult;
-    int j;
-    int i = 0;
-    while (i <= 9)
-    {
-        j = 0;
-        while (j <= 9)
-        {
-            mult = i * j;
-            if (j == 0)
-            {
-                _putchar('0' + mult);
-            }
-            else if (mult <= 9)
-            {
-                _putchar(' ');
-                _putchar('0' + mult);
-            }
-            else
-            {
-                _putchar('0' + mult / 10);
-                _putchar('0' + mult % 10);
-            }
-            if (j < 9)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-            j++;
-        }
-        _putchar('\n');
-        i++;
-    }
+int mult;
+int j;
+int i = 0;
+while (i <= 9)
+{
+j = 0;
+while (j <= 9)
+{
+mult = i * j;
+if (j == 0)
+{
+_putchar('0' + mult);
+}
+else if (mult <= 9)
+{
+_putchar(' ');
+_putchar('0' + mult);
+}
+else
+{
+_putchar('0' + mult / 10);
+_putchar('0' + mult % 10);
+}
+if (j < 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+j++;
+}
+_putchar('\n');
+i++;
+}
 }
