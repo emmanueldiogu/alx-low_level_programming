3fdff37653701c874350592461f4760fd02a2e7f
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-09-15 10:38:44 +0100

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ *
+ * Description: Function that adds 2 intergers,
+ * and returns result
+ * Result: 0
+*/
+int add(int a, int b)
+{
+    int c = a + b;
+    return (c);
+}
