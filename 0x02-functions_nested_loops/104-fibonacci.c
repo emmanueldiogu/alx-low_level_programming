af4b717e15d340fd32357d913e5e7197ff0f0b74
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-09-18 20:04:40 +0100

    fibonacci 4

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:   that finds and prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2, followed by a new line.
+ * Return: 0
+*/
+
+int main(void)
+{
+int i = 0;
+unsigned long int a = 0, b = 1, next = 0;
+
+while (i < 98)
+{
+next = a + b;
+a = b;
+b = next;
+printf("%lu", next);
+
+if (i < 97)
+printf(", ");
+i++;
+}
+putchar('\n');
+return (0);
+}
