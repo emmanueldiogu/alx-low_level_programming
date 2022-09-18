ffb0c726151d27acc7b00a0c9169ead9492d7148
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-09-18 19:07:26 +0100

    print times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,18 +6,14 @@
  *
  * Description: Function to print the n times table,
  * starting with 0
- * Return: 0 
+ * Return: 0
 */
 
 void print_times_table(int n)
 {
 int i = 0, mult, j;
-
 if (n < 0 || n > 15)
-{
 return;
-}
-
 while (i <= n)
 {
 j = 0;
@@ -56,5 +52,4 @@ j++;
 _putchar('\n');
 i++;
 }
-
 }
