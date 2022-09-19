ce133d03006a226e2e6a3b7218132c8ff84d1004
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-09-19 02:39:54 +0100

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,18 +5,19 @@
  * @c: character to evaluate
  *
  * Description: Function to check for uppercase chatacter
+ *
  * Return: 0
 */
 
 int _isupper(int c)
 {
-    if (c >= 65 && c <= 90)
-    {
-        return (1);
-    }
-    else if (c >= 97 && c <= 122)
-    {
-        return (0);
-    }
-    return (0);
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else if (c >= 97 && c <= 122)
+{
+return (0);
+}
+return (0);
 }
