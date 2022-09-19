739f145b7ff16726cc409f77f0b79eaef92e954c
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-09-19 02:48:47 +0100

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit - Entry point
+ * @c: entry parameter
+ *
+ * Description: Function that checks for a digit
+ * 
+ * Return: 0
+*/
+
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+
+return (0);
+}
