ccb30f8b9bd2128d7be6e0d2c7462fe6649972c4
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-09-29 04:54:33 +0100

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,10 +1,22 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * @a: description
+ * factorial - Entry point
+ * @n: int
  *
- * Description: function
+ * Description: function that returns the factorial of a given number.
  *
  * Return: 0
  */
+int factorial(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+else if (n == 0)
+{
+return (1);
+}
+return (n * factorial(n - 1));
+}
