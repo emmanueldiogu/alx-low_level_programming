821ab99568ea7ec368ac705ac964354ccd1d784d
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-09-29 05:53:00 +0100

    is prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,10 +1,33 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * @a: description
+ * is_prime_number - Entry point
+ * @n: int
  *
- * Description: function
- *
+ * Description: function that returns 1 if the input integer is a prime number,
+ * otherwise return 0.
  * Return: 0
  */
+int is_prime_number(int n)
+{
+return (is_prime(n, 2));
+}
+
+/**
+ * is_prime - Entry point
+ * @n: int
+ * @y: int
+ *
+ * Description: function that returns 1 if the input integer is a prime number,
+ * otherwise return 0.
+ * Return: 0
+ */
+int is_prime(int n, int y)
+{
+if (y >= n && n > 1)
+return (1);
+else if (n % y == 0 || n <= 1)
+return (0);
+else
+return (is_prime(n, y + 1));
+}
