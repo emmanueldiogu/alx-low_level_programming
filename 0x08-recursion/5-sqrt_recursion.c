effd71f80fafb513a10764ac4b69cbd806878772
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-09-29 05:26:31 +0100

    sqrt recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,10 +1,36 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * @a: description
+ * _sqrt_recursion - Entry point
+ * @n: int
  *
- * Description: function
+ * Description: function that returns natural square root of a number.
  *
  * Return: 0
  */
+int _sqrt_recursion(int n)
+{
+return (square(n, 1));
+}
+
+/**
+ * square - Entry point
+ * @n: int entry
+ * @y: interval
+ *
+ * Description: function that returns natural square root of a number.
+ *
+ * Return: 0
+ */
+int square(int n, int y)
+{
+if (y * y == n)
+{
+return (y);
+}
+else if (y * y < n)
+{
+return (square(n, y + 1));
+}
+return (-1);
+}
