53e7f2a6a34db1c67168bb4aa2f73b104f875e08
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-10-03 15:52:42 +0100

    multiply values

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: arguments string
+ *
+ * Description: program that prints its name, followed by a new line
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, result, input;
+
+	i = 1;
+	result = 1`;
+
+	while (i < argc)
+	{
+		input = strtol(argv[i], NULL, 10);
+		result *= input;
+		i++;
+	}
+		printf("%d\n", result);
+
+	return (0);
+}
