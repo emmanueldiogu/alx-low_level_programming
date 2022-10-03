b1dc13f2e94579a2ac07ab4cecd9e8c32e54e6f3
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-10-03 14:35:00 +0100

    my name is___

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: arguments string
+ *
+ * Description: program that prints its name, followed by a new line
+ * Result: 0
+ */
+int main (int argc, char* argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
