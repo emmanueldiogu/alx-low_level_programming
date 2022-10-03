3b557cc8fdedc2a40ed4194968a4a6416c7ad631
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-10-03 19:33:56 +0100

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <ctype.h>
+/**
+ * _isDigit - checks if input is int
+ * @s: check param
+ * Return: 0
+ */
+int _isDigit(char *s)
+{
+	int i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (1);
+		i++;
+	}
+	return (0);
+}
 
 /**
  * main - Entry point
@@ -17,22 +32,15 @@ int main(int argc, char *argv[])
 
 	result = 0;
 
-	if (argc <= 1)
-	{
-		printf("%d\n", result);
-		return (1);
-	}
-
 	for (i = 1; i < argc; i++)
 	{
 		input = strtol(argv[i], NULL, 10);
-		if (input <= 0)
+		if (_isDigit(argv[i]))
 		{
 			printf("Error\n");
 			return (1);
-			break;
 		}
-		
+
 		result += input;
 	}
 
