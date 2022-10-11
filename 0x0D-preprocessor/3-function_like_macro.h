5e562bee310b969d9ad1893538463651a70b69ae
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-10-11 10:48:13 +0100

    preprocessor

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,8 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
-/**
- * using tinery operation instead of if else
-*/
+/* using tinery operation instead of if else */
 #define ABS(x) (x > 0 ? (x) : (x) * -1)
 
 #endif /* FUNCTION_LIKE_MACRO_H */
