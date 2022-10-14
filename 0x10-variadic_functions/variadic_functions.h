ce8c45cba56f9907c85b43efdef54555cec74629
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-10-14 03:07:39 +0100

    variadic

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,27 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+/**
+ * struct type - Struct data type
+ *
+ * @op: data type argument
+ * @f: The function associated
+ */
+
+typedef struct type
+{
+	char *op;
+	void (*f)(va_list, char *);
+} type_t;
+
+#endif /* VARIADIC_FUNCTIONS_H */
