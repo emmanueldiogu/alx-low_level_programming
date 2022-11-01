036b0079ce5d67cdc054d4149caa7f1b94383d09
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-11-01 03:12:13 +0100

    submit 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,23 +8,24 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int d, status, i;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-		return (1);
 
-	d = open(filename, O_APPEND | O_WRONLY);
-	if (f == -1)
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
 		return (-1);
 
-	for (i = 0; text_content[i] != '\0'; i++)
-		;
-	status = write(d, text_content, i);
-	if (status == -1)
-		return (-1);
+	close(0);
 
-	close(d);
 	return (1);
 }
