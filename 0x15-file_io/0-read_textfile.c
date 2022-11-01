c18cec49b4f0e882a7aee6722a94457a16bf94e3
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-11-01 02:19:52 +0100

    submit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,32 +11,29 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int d;
-	ssize_t rcount, wcount;
-	char *buffer;
+int d;
+ssize_t rcount, wcount;
+char *buffer;
 
-	if (filename == NULL)
-		return (0);
+if (filename == NULL)
+return (0);
 
-	d = open(filename, O_RDWR);
-	if (d == -1)
-		return (0);
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
+return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-	{
-		free(buffer);
-		return (0);
-	}
-	rcount = read(d, buffer, letters);
-	if (rcount == -1)
-		return (0);
+d = open(filename, O_RDONLY);
+rcount = read(d, buffer, letters);
+wcount = write(STDOUT_FILENO, buffer, rcount);
 
-	wcount = write(STDOUT_FILENO, buffer, rcount);
-	if (wcount == -1 || rcount != wcount)
-		return (0);
-	free(buffer);
+if (d == -1 || rcount == -1 || wcount == -1 || wcount != rcount)
+{
+free(buffer);
+return (0);
+}
 
-	close(d);
-	return (wcount);
+free(buffer);
+close(d);
+
+return (wcount);
 }
