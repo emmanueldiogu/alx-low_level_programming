cc99658101cde6011ff3a2ca7025da4f4784bdfb
Emmanuel Diogu <emmanueldiogu@gmail.com>
2023-03-17 09:07:15 +0100

    advanced

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -4,27 +4,28 @@
  * find_listint_loop - finds the start node of a loop in a linked list
  * @head: pointer to the head of the linked list
  *
- * Return: the address of the node where the loop starts, or NULL if there is no loop
+ * Return: the address of the node where the loop starts,
+ * or NULL if there is no loop
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *turtle = head, *hare = head;
+listint_t *turtle = head, *hare = head;
 
-    while (turtle && hare && hare->next)
-    {
-        turtle = turtle->next;
-        hare = hare->next->next;
-        if (turtle == hare)
-        {
-            turtle = head;
-            while (turtle != hare)
-            {
-                turtle = turtle->next;
-                hare = hare->next;
-            }
-            return hare;
-        }
-    }
+while (turtle && hare && hare->next)
+{
+turtle = turtle->next;
+hare = hare->next->next;
+if (turtle == hare)
+{
+turtle = head;
+while (turtle != hare)
+{
+turtle = turtle->next;
+hare = hare->next;
+}
+return (hare);
+}
+}
 
-    return NULL;
+return (NULL);
 }
