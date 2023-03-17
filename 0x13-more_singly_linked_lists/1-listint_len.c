68f238565cb1fb21c3515a2e5c55a427d13def94
Emmanuel Diogu <emmanueldiogu@gmail.com>
2023-03-17 07:24:36 +0100

    upadte Pointer to the head of the list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -4,6 +4,7 @@
  * print_listint - Prints all the elements of a listint_t list
  * @h: Pointer to the head of the list
  *
+ * Description - prints number of nodes
  * Pointer to the head of the list
  */
 size_t print_listint(const listint_t *h)
