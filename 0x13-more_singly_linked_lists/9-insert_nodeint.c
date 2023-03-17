fc074e53b4084d6e8a6c21683cacbd56f9a98026
Emmanuel Diogu <emmanueldiogu@gmail.com>
2023-03-17 08:17:08 +0100

    3-8-sum

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,11 +15,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 
 	if (!head)
-		return NULL;
+		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
 	if (!new_node)
-		return NULL;
+		return (NULL);
 
 	new_node->n = n;
 
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new_node->next = *head;
 		*head = new_node;
-		return new_node;
+		return (new_node);
 	}
 
 	current_node = *head;
@@ -38,11 +38,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (i != idx - 1 || !current_node)
 	{
 		free(new_node);
-		return NULL;
+		return (NULL);
 	}
 
 	new_node->next = current_node->next;
 	current_node->next = new_node;
 
-	return new_node;
+	return (new_node);
 }
